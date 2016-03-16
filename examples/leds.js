57ce2e617ca28b48607fc4a4dc942c7006143c06
Julien Fauville <fauville.j@gmail.com>
2016-03-16 18:09:21 +0100

    add pin lib

---
+++ examples/leds.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const Pin = require('../lib/pin');
+
+
+const leds = [
+  new Pin(4),
+  new Pin(17),
+  new Pin(27),
+  new Pin(22),
+  new Pin(5),
+  new Pin(6),
+  new Pin(13),
+  new Pin(19)
+];
+var i = 0;
+
+leds.forEach((led) => { led.up(); });
+
+setTimeout(() => {
+  leds.forEach((led) => { led.down(); });
+  setInterval(() => {
+    if (i > 7) i = 0;
+    let led = leds[i];
+    led.up();
+    if (i > 0) leds[i-1].down();
+    i++;
+  }, 500);
+}, 1000);
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 'use strict';
 
 module.exports = require('./lib/gpio');
+module.exports.Pin = require('./lib/pin');
